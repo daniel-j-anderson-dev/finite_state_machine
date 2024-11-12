57d9010dc48958b69275cfccdf89c2bcf5b846f3
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-11-11 18:50:29 -0700

    cargo fmt

--- examples/rock_paper_scissors.rs
+++ examples/rock_paper_scissors.rs
@@ -1,14 +1,17 @@
+use finite_state_machine::rock_paper_scissors::{
+    self, Input as RockPaperScissorsInput, Output as RockPaperScissorsOutput,
+};
 use std::io::{stdin, stdout, Write};
-use finite_state_machine::rock_paper_scissors::{self, Input as RockPaperScissorsInput, Output as RockPaperScissorsOutput};
 
 fn main() -> Result<(), std::io::Error> {
-    let mut rock_paper_scissors_finite_state_machine = rock_paper_scissors::new_finite_state_machine();
+    let mut rock_paper_scissors_finite_state_machine =
+        rock_paper_scissors::new_finite_state_machine();
     let mut prompt = "Player one, please input one of the following.\nRock\nPaper\nScissors\n> ";
 
     loop {
         let input = get_parsed_input::<RockPaperScissorsInput>(prompt)?;
         let output = rock_paper_scissors_finite_state_machine.transition(&input);
-        
+
         prompt = "Player two, please input one of the following.\nRock\nPaper\nScissors\n> ";
 
         print("\n")?;
@@ -18,15 +21,15 @@ fn main() -> Result<(), std::io::Error> {
             RockPaperScissorsOutput::Player1Wins => {
                 print("Player one wins\n")?;
                 break;
-            },
+            }
             RockPaperScissorsOutput::Player2Wins => {
                 print("Player two wins!\n")?;
                 break;
-            },
+            }
             RockPaperScissorsOutput::Tie => {
                 print("It's a tie!\n")?;
                 break;
-            },
+            }
         }
     }
 
@@ -41,11 +44,7 @@ pub fn print(message: &str) -> Result<(), std::io::Error> {
 }
 
 pub fn get_input(prompt: &str) -> Result<String, std::io::Error> {
-    {
-        let mut stdout = stdout();
-        stdout.write_all(prompt.as_bytes())?;
-        stdout.flush()?;
-    }
+    print(prompt)?;
 
     let mut input = String::new();
     stdin().read_line(&mut input)?;
